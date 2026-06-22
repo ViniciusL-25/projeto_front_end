50d7c37b9faa20b1993a63cff6e07b9d86e02f50
Vinicius Leoncio <viniciusleoncio87@gmail.com>
2026-06-21 22:37:06 -0300

    Adicionado delete e editar

--- cadastro-alunos/src/pages/Login.jsx
+++ cadastro-alunos/src/pages/Login.jsx
@@ -1,16 +1,50 @@
 import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Login.css';
-import '../pages/Cadastro';
+
 
 
 
 function Login(){
-    const { register, handleSubmit, formState: { errors } } = useForm()
-    const entrar = data => {
-        console.log("Dados enviados", data)
-    }
+
+    const navigate = useNavigate();
     
+
+    const { register, handleSubmit, formState: { errors } } = useForm()
+    const entrar = async (data) => {
+    const response = await fetch('http://localhost:3000/alunos');
+    const alunos = await response.json();
+        
+    console.log("Dados Digitados:", data)
+    console.log("Alunos da API:", alunos)
+    const usuario = alunos.find((aluno) => {
+
+        return(
+
+        aluno.email === data.email &&
+        aluno.senha === data.senha
+
+        );
+        
+        
+    });
+    
+    console.log("Usuario não encontrado",usuario);
+    
+    if (usuario) {
+        console.log("Login válido");
+        localStorage.setItem(
+            "usuarioLogado",
+            JSON.stringify(usuario)
+        );
+         navigate('/home');
+    } else {
+        alert("Login não encontrado");
+    }
+}
+    
+   
     return(
 <div className="login-container">
     <div className="body-card" >
@@ -25,6 +59,7 @@ function Login(){
                     <div className="body" >
 
                         <form onSubmit={handleSubmit(entrar)}>
+                           
 
                             <div className="fields-email">
                                 <label>Email</label>
